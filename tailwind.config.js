1230845b2a8b1bea426ab73709cacecd97c269de
Jens <jens@vragments.com>
2023-01-11 21:34:20 +0100

    tabwidth & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'], // add your preferred file extensions here
-	theme: {
-		extend: {
-			fontFamily: {
-				'playfair-display': ['"Playfair Display"'],
-				glegoo: ['Glegoo'],
-			},
-		},
-	},
-	plugins: [],
+    content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"], // add your preferred file extensions here
+    theme: {
+        extend: {
+            fontFamily: {
+                "playfair-display": ['"Playfair Display"'],
+                glegoo: ["Glegoo"],
+            },
+        },
+    },
+    plugins: [],
 };
