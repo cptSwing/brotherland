1f6e35a2d762b47f11da09e4ff3f1c082567ff6b
Jens <jens@vragments.com>
2023-03-15 16:36:49 +0100

    interview styles, img popup, resize script

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+/** use me in body.onload */
+function setParentElementHeight(childElementClass) {
+    const childElementsCollection = document.getElementsByClassName(childElementClass);
+
+    const childElementsArr = [...childElementsCollection];
+
+    childElementsArr.forEach((childElem) => {
+        const parentElement = childElem.parentElement;
+
+        // @ts-ignore
+        const childElementHeight = childElem.offsetHeight;
+
+        parentElement && parentElement.setAttribute("style", `height: ${childElementHeight}px`);
+    });
+}
+
+function imageHref(imageClass) {
+    const imageElementsCollection = document.getElementsByClassName(imageClass);
+
+    const imageElementsArr = [...imageElementsCollection];
+
+    imageElementsArr.forEach((image) => {
+        image.addEventListener("click", (ev) => {
+            // @ts-ignore
+            ev.target && window.open(ev.target.src, "_blank");
+        });
+    });
+}
