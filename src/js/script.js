f64a3cecb641d77fe7c63dc61aab498f66667bdb
Jens <jens@vragments.com>
2023-06-22 11:43:27 +0200

    ...

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,53 @@
+import SimpleLightbox from "simplelightbox";
+
+exhibitGalleryOnLoad();
+
 /** use me in body.onload */
-function setParentElementHeight(childElementClass) {
-    const childElementsCollection = document.getElementsByClassName(childElementClass);
+function setParentElementHeight( childElementClass ) {
+    const childElementsCollection = document.getElementsByClassName( childElementClass );
 
-    const childElementsArr = [...childElementsCollection];
+    const childElementsArr = [ ...childElementsCollection ];
 
-    childElementsArr.forEach((childElem) => {
+    childElementsArr.forEach( ( childElem ) => {
         const parentElement = childElem.parentElement;
 
         // @ts-ignore
         const childElementHeight = childElem.offsetHeight;
 
-        parentElement && parentElement.setAttribute("style", `height: ${childElementHeight}px`);
-    });
+        parentElement && parentElement.setAttribute( "style", `height: ${ childElementHeight }px` );
+    } );
 }
 
-function imageHref(imageClass) {
-    const imageElementsCollection = document.getElementsByClassName(imageClass);
+function imageHref( imageClass ) {
+    const imageElementsCollection = document.getElementsByClassName( imageClass );
 
-    const imageElementsArr = [...imageElementsCollection];
+    const imageElementsArr = [ ...imageElementsCollection ];
 
-    imageElementsArr.forEach((image) => {
-        image.addEventListener("click", (ev) => {
+    imageElementsArr.forEach( ( image ) => {
+        image.addEventListener( "click", ( ev ) => {
             // @ts-ignore
-            ev.target && window.open(ev.target.src, "_blank");
-        });
-    });
+            ev.target && window.open( ev.target.src, "_blank" );
+        } );
+    } );
+}
+
+function exhibitGalleryOnLoad() {
+    let gallery = new this.SimpleLightbox( ".gallery a", { uniqueImages: false } );
+
+    const data = gallery.getLighboxData()
+    console.log(data);
+
+    gallery.on( "click", () => {
+        console.log( "click" );
+        gallery.open()
+    } );
+
+    gallery.on( 'show.simplelightbox', function () {
+        console.log( 'show.simplelightbox' );
+    } );
+
+
+    gallery.on( 'error.simplelightbox', function ( e ) {
+        console.log( e ); // some usefull information
+    } );
 }
