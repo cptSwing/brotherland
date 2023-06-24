cb5256352fbb716d8d1e9f5485913f2b0982aa86
Jens <jens@vragments.com>
2023-06-24 19:04:20 +0200

    VA

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"], // add your preferred file extensions here
+    content: [ "./src/**/*.{html,js,jsx,ts,tsx,vue}" ], // add your preferred file extensions here
     theme: {
         extend: {
             fontFamily: {
-                "playfair-display": ['"Playfair Display"'],
-                glegoo: ["Glegoo"],
-                "crimson-text": ['"Crimson Text"'],
+                "playfair-display": [ '"Playfair Display"' ],
+                glegoo: [ "Glegoo" ],
+                "crimson-text": [ '"Crimson Text"' ],
+                "roboto": [ 'Roboto' ]
+            },
+            aspectRatio: {
+                '2/3': '2 / 3',
+                '3/2': '3 / 2'
             },
         },
     },
