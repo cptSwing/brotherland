1cd8978b5ff6ac31e9f9e0cadc5c520db30ba58c
Jens <jens@vragments.com>
2023-07-30 17:33:23 +0200

    gallery edits

--- src/js/script.js
+++ src/js/script.js
@@ -33,12 +33,15 @@ exhibitGalleryOnLoad();
 
 
 function exhibitGalleryOnLoad() {
-    new SimpleLightbox(".gallery img", {
-        sourceAttr: "src",
+    new SimpleLightbox(".gallery a", {
+        // sourceAttr: "src",
         animationSpeed: 100,
         fadeSpeed: 0,
         uniqueImages: false,
         overlayOpacity: 1.0,
-        className: "my-auto",
+        // className: "my-auto",
+        // captionSelector: "self",
+        // captionPosition: "outside",
+        captionClass: /* tw */ "!text-sm !font-crimson-text !bg-black/50",
     });
 }
