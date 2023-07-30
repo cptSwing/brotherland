5999f6c102b713c0a97dfd16f3b7b4b24048b1b5
Jens <jens@vragments.com>
2023-07-30 02:10:15 +0200

    moar

--- src/js/script.js
+++ src/js/script.js
@@ -2,41 +2,43 @@ import SimpleLightbox from "simplelightbox";
 
 exhibitGalleryOnLoad();
 
-/** use me in body.onload */
-function setParentElementHeight( childElementClass ) {
-    const childElementsCollection = document.getElementsByClassName( childElementClass );
+// /** use me in body.onload */
+// function setParentElementHeight( childElementClass ) {
+//     const childElementsCollection = document.getElementsByClassName( childElementClass );
 
-    const childElementsArr = [ ...childElementsCollection ];
+//     const childElementsArr = [ ...childElementsCollection ];
 
-    childElementsArr.forEach( ( childElem ) => {
-        const parentElement = childElem.parentElement;
+//     childElementsArr.forEach( ( childElem ) => {
+//         const parentElement = childElem.parentElement;
 
-        // @ts-ignore
-        const childElementHeight = childElem.offsetHeight;
+//         // @ts-ignore
+//         const childElementHeight = childElem.offsetHeight;
 
-        parentElement && parentElement.setAttribute( "style", `height: ${ childElementHeight }px` );
-    } );
-}
+//         parentElement && parentElement.setAttribute( "style", `height: ${ childElementHeight }px` );
+//     } );
+// }
 
-function imageHref( imageClass ) {
-    const imageElementsCollection = document.getElementsByClassName( imageClass );
+// function imageHref( imageClass ) {
+//     const imageElementsCollection = document.getElementsByClassName( imageClass );
 
-    const imageElementsArr = [ ...imageElementsCollection ];
+//     const imageElementsArr = [ ...imageElementsCollection ];
 
-    imageElementsArr.forEach( ( image ) => {
-        image.addEventListener( "click", ( ev ) => {
-            // @ts-ignore
-            ev.target && window.open( ev.target.src, "_blank" );
-        } );
-    } );
-}
+//     imageElementsArr.forEach( ( image ) => {
+//         image.addEventListener( "click", ( ev ) => {
+//             // @ts-ignore
+//             ev.target && window.open( ev.target.src, "_blank" );
+//         } );
+//     } );
+// }
 
 
 function exhibitGalleryOnLoad() {
-    new SimpleLightbox( ".gallery img", {
-        sourceAttr: 'src',
-        animationSpeed: 150,
+    new SimpleLightbox(".gallery img", {
+        sourceAttr: "src",
+        animationSpeed: 100,
+        fadeSpeed: 0,
         uniqueImages: false,
-        overlayOpacity: 1.0
-    } );
+        overlayOpacity: 1.0,
+        className: "my-auto",
+    });
 }
