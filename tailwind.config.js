309e4a2ad7b378d7fd7647560cd34a5d4cd3c60e
Jens <jens@vragments.com>
2023-08-03 10:36:22 +0200

    added tailwind breakpointInspector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    content: [ "./src/**/*.{html,js,jsx,ts,tsx,vue}" ], // add your preferred file extensions here
+    content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"], // add your preferred file extensions here
     theme: {
         extend: {
             fontFamily: {
-                "playfair-display": [ '"Playfair Display"' ],
-                glegoo: [ "Glegoo" ],
-                "crimson-text": [ '"Crimson Text"' ],
-                "roboto": [ 'Roboto' ]
+                "playfair-display": ['"Playfair Display"'],
+                glegoo: ["Glegoo"],
+                "crimson-text": ['"Crimson Text"'],
+                roboto: ["Roboto"],
             },
             aspectRatio: {
-                '2/3': '2 / 3',
-                '3/2': '3 / 2'
+                "2/3": "2 / 3",
+                "3/2": "3 / 2",
             },
         },
     },
-    plugins: [],
+    plugins: [require("./tailwind_BreakpointInspector")],
 };
