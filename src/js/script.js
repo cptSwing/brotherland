99508cf715c7c01dee2bed0e140ff94a4df793a6
Jens <jens@vragments.com>
2023-08-06 19:17:43 +0200

    mobile changes

--- src/js/script.js
+++ src/js/script.js
@@ -2,36 +2,6 @@ import SimpleLightbox from "simplelightbox";
 
 exhibitGalleryOnLoad();
 
-// /** use me in body.onload */
-// function setParentElementHeight( childElementClass ) {
-//     const childElementsCollection = document.getElementsByClassName( childElementClass );
-
-//     const childElementsArr = [ ...childElementsCollection ];
-
-//     childElementsArr.forEach( ( childElem ) => {
-//         const parentElement = childElem.parentElement;
-
-//         // @ts-ignore
-//         const childElementHeight = childElem.offsetHeight;
-
-//         parentElement && parentElement.setAttribute( "style", `height: ${ childElementHeight }px` );
-//     } );
-// }
-
-// function imageHref( imageClass ) {
-//     const imageElementsCollection = document.getElementsByClassName( imageClass );
-
-//     const imageElementsArr = [ ...imageElementsCollection ];
-
-//     imageElementsArr.forEach( ( image ) => {
-//         image.addEventListener( "click", ( ev ) => {
-//             // @ts-ignore
-//             ev.target && window.open( ev.target.src, "_blank" );
-//         } );
-//     } );
-// }
-
-
 function exhibitGalleryOnLoad() {
     new SimpleLightbox(".gallery a", {
         // sourceAttr: "src",
