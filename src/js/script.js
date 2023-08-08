f2d1ce0e33d9e6c5e6ae6f6018bb153d24d0f4b3
Jens <jens@vragments.com>
2023-08-08 15:34:57 +0200

    gallery touch bugs fixed, nav text scalable

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,37 @@
+// @ts-ignore
 import SimpleLightbox from "simplelightbox";
 
 exhibitGalleryOnLoad();
 
 function exhibitGalleryOnLoad() {
-    new SimpleLightbox(".gallery a", {
-        // sourceAttr: "src",
+    const gallery = new SimpleLightbox("#simplelightboxgallery a", {
         animationSpeed: 100,
+        animationSlide: false,
         fadeSpeed: 0,
         uniqueImages: false,
         overlayOpacity: 1.0,
-        // className: "my-auto",
-        // captionSelector: "self",
-        // captionPosition: "outside",
+        swipeClose: false,
+        scrollZoom: false,
         captionClass: /* tw */ "!text-sm !font-crimson-text !bg-black/50",
+    });
+
+    // @ts-ignore
+    gallery.on("shown.simplelightbox", function () {
+        const galleryGrid = document.getElementById("simplelightboxgallery");
+        const hamburgerLabel = document.getElementById("hamburger-menu-label");
+
+        galleryGrid?.classList.add("pointer-events-none");
+        hamburgerLabel?.classList.add("pointer-events-none");
+    });
+
+    // @ts-ignore
+    gallery.on("closed.simplelightbox", function () {
+        const galleryGrid = document.getElementById("simplelightboxgallery");
+        const hamburgerLabel = document.getElementById("hamburger-menu-label");
+
+        setTimeout(() => {
+            galleryGrid?.classList.remove("pointer-events-none");
+            hamburgerLabel?.classList.remove("pointer-events-none");
+        }, 500);
     });
 }
