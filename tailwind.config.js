39d94f71b973f7657ee7d92fd799062d26bb3fb3
Jens <jens@vragments.com>
2023-08-17 14:27:19 +0200

    upgrades

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,6 @@ module.exports = {
             },
         },
     },
-    plugins: [require("./tailwind_BreakpointInspector")],
+    // plugins: [require("./tailwind_BreakpointInspector")],
+    plugins: [require("tailwindcss-breakpoints-inspector")],
 };
