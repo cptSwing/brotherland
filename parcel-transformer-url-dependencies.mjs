f1852f1673cade0436565b01ea2f17b02ab48bcc
cptSwing <jens@jbrandenburg.de>
2026-05-20 19:52:30 +0200

    parcel url transformer

---
+++ parcel-transformer-url-dependencies.mjs
@@ -0,0 +1,60 @@
+import { Transformer } from "@parcel/plugin";
+import { parser } from "posthtml-parser";
+import { render } from "posthtml-render";
+import PostHTML from "posthtml";
+
+const config = {
+    "audio-player": ["source"],
+};
+
+const transformerAudioPlayer = new Transformer({
+    canReuseAST({ ast }) {
+        return ast.type === "posthtml";
+    },
+
+    async parse({ asset }) {
+        return {
+            type: "posthtml",
+            version: "0.16.6",
+            program: parser(await asset.getCode(), {
+                lowerCaseAttributeNames: true,
+            }),
+        };
+    },
+
+    async transform({ asset }) {
+        const code = await asset.getCode();
+
+        const result = await PostHTML([
+            (tree) => {
+                tree.walk((node) => {
+                    if (Object.keys(config).includes(node.tag) && node.attrs) {
+                        config[node.tag].forEach((attrName) => {
+                            const nodeAttribute = node.attrs[attrName];
+
+                            if (nodeAttribute) {
+                                node.attrs[attrName] = asset.addURLDependency(nodeAttribute, {
+                                    specifierType: "url",
+                                });
+                            }
+                        });
+                    }
+
+                    return node;
+                });
+            },
+        ]).process(code);
+
+        asset.setCode(result.html);
+
+        return [asset];
+    },
+
+    async generate({ ast }) {
+        return {
+            content: render(ast.program),
+        };
+    },
+});
+
+export default transformerAudioPlayer;
